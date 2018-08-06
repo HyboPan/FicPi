132ab3977b29a42d1c5d2099c0cd039aa096fb60
HyboPan <41903468+HyboPan@users.noreply.github.com>
2018-08-06 13:27:22 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -34,22 +34,22 @@ class Config:
 
     #配置文件读取内部函数，并保存到 config 这个字典里
     def _read_config(self):
-        con = {}
-        pre = 0
+        con = {'s': 0}
         test = Args()
         try:
             with open(test.c, 'r') as cfg_file:
                 for cfg_line in cfg_file:
-                    con[cfg_line.split(' = ')[0].strip()] = float(cfg_line.split(' = ')[1].strip())
-                    pre += float(cfg_line.split(' = ')[1].strip())
-            JiShuL = float(config['JiShuL'])
-            JiShuH = float(config['JiShuH'])
-            print(JiShuL, JiShuH, con)
+                    a = cfg_line.split(' = ')[0].strip()
+                    b = cfg_line.split(' = ')[1].strip()
+                    if a == 'JiShuL' or a == 'JiShuH':
+                        con[a] = b
+                    else:
+                        con['s'] += float(b)
         except:
             print("Parameter Error2")
             sys.exit(-1)
 
-        return con and pre and JiShuL and JiShuH
+        return con
     
     
 # 用户数据类
@@ -76,73 +76,68 @@ class UserData:
         return userdata
 
     
-# 税后工资计算类
-class IncomeTaxCalculator:
-
-    # 计算每位员工的税后工资函数
-    def calc(self):
-        udata = UserData()
-        conf = Config()
-        conf.con
-        conf.JiShuL
-        conf.JiShuH
-        conf.pre
-        for usd in udata.userdata:
-            wn = usd[0]
-            wage = usd[1]
-            if wage <= JiShuL:
-                insurance = JiShuL * pre
-                pay = 0
-            elif JiShuL < wage <= 3500:
-                insurance = wage * pre
-                pay = 0
-            elif JiShuH >= wage > 3500:
-                taxincome = wage - 3500 - wage * pre
-                if taxincome <= 1500:
-                    tr = 0.03
-                    ed = 0
-                elif 1500 < taxincome <= 4500:
-                    tr = 0.10
-                    ed = 105
-                elif 4500 < taxincome <= 9000:
-                    tr = 0.20
-                    ed = 555
-                elif 9000 < taxincome <= 35000:
-                    tr = 0.25
-                    ed = 1005
-                elif 35000 < taxincome <= 55000:
-                    tr = 0.30
-                    ed = 2755
-                elif 55000 < taxincome <= 80000:
-                    tr = 0.35
-                    ed = 5505
-                elif 80000 < taxincome:
-                    tr = 0.45
-                    ed = 13505
-                insurance = wage * pre
-                pay = taxincome * tr -ed
-            elif JiShuH < wage:
-                insurance = JiShuH * pre
-                taxincome = wage - 3500 - insurance
+# 税后工资计算
+def calc():
+    udata = UserData()
+    conf = Config()
+    conf.config
+    JiShuL = float(con['JiShuL'])
+    JiShuH = float(con['JiShuH'])
+    pre = float(con['s'])
+    for usd in udata.userdata:
+        wn = usd[0]
+        wage = usd[1]
+        if wage <= JiShuL:
+           insurance = JiShuL * pre
+           pay = 0
+        elif JiShuL < wage <= 3500:
+            insurance = wage * pre
+            pay = 0
+        elif JiShuH >= wage > 3500:
+            taxincome = wage - 3500 - wage * pre
+            if taxincome <= 1500:
+                tr = 0.03
+                ed = 0
+            elif 1500 < taxincome <= 4500:
+                tr = 0.10
+                ed = 105
+            elif 4500 < taxincome <= 9000:
+                tr = 0.20
+                ed = 555
+            elif 9000 < taxincome <= 35000:
+                tr = 0.25
+                ed = 1005
+            elif 35000 < taxincome <= 55000:
+                tr = 0.30
+                ed = 2755
+            elif 55000 < taxincome <= 80000:
+                tr = 0.35
+                ed = 5505
+            elif 80000 < taxincome:
                 tr = 0.45
                 ed = 13505
-                pay = taxincome * tr -ed
-            afwage = wage - pay - insurance
-            return afwage and pay and insurance
-            print(format(wn) + "," + format(wage) + "," + format(insurance, ".2f") + "," + format(pay, ".2f") + format(afwage, ".2f"))
+            insurance = wage * pre
+            pay = taxincome * tr -ed
+        elif JiShuH < wage:
+            insurance = JiShuH * pre
+            taxincome = wage - 3500 - insurance
+            tr = 0.45
+            ed = 13505
+            pay = taxincome * tr -ed
+        afwage = wage - pay - insurance
+        return afwage and pay and insurance
+        print(format(wn) + "," + format(wage) + "," + format(insurance, ".2f") + "," + format(pay, ".2f") + format(afwage, ".2f"))
 
-    # 输出 CSV 文件函数
-    def export(self):
-        a = IncomeTaxCalculator()   
-        result = a.calc()
-        gongzi = Args()
-        with open(gongzi.o, 'w') as f:
-            writer = csv.writer(f)
-            writer.writerows(result)
+# 输出 CSV 文件函数
+def export():  
+    result = calc()
+    gongzi = Args()
+    with open(gongzi.o, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(result)
 
 
 if __name__ == "__main__":
-    i = IncomeTaxCalculator()
-    i.export()
+    export()
    
     
